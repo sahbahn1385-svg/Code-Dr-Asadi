786d027b3d88bc1e3b3159150052a5cc57ba2bf6
sahbahn1385-svg <sahbahn1385@gmail.com>
2026-02-06 18:52:10 +0330

    Add files via upload

---
+++ Codes 5.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  6 18:43:39 2026
+
+@author: applegolshan
+"""
+"""
+gereh ha
+"""
+class node :
+    def __init__(self , d):
+        self.Data = d
+        self.next = None
+
+
+
+
+class linked_list :
+    def __init__(self):
+        self.head = None
+    def insert_frist(self , x):
+        if self.head == None:
+            self.head = node(x)
+        else:
+            a = node(x)
+            a.next = self.head
+            self.head = a     
+    def insert_last(self , x):
+        if self.head is None:
+            self.head = node(x)
+        else:
+            a = node(x)
+            c = self.head
+            while c.next:
+                c = c.next 
+            c.next = a
+    def insert_after(self , x, y):
+        if self.head is None:
+            print("list is empty")
+        else:
+            c = self.head
+            while c:
+                if c.Data == x:
+                    a = node(y)
+                    a.next = c.next
+                    c.next = a
+                c = c.next
+            print("not found")
+    def insert_after(self , x, y):
+        if self.head is None:
+            print("list is empty")
+        else:
+            c = self.head
+            while c:
+                if c.Data == x:
+                    a = node(y)
+                    a.next = c.next
+                    c.next = a
+                    return
+                c = c.next
+            print("not found")    
+
+    def insert_after(self , x, y):
+        if self.head is None:
+            print("list is empty")
+            return
+        if self.head.Data == x:
+            self.insert_frist(y)
+            return
+        c = self.head
+        while c.next:
+                if c.next.Data == x:
+                    a = node(y)
+                    a.next = c.next
+                    c.next = a
+                    return
+                c = c.next
+                print("not found")                      
+
+    def insert_befor(self , x, y):
+        if self.head is None:
+            print("list is empty")
+            return
+        if self.head.Data == x:
+            self.insert_frist(y)
+            return
+    while c.next:
+            if c.next.Data == x:
+                a = node(y)
+                a.next = c.next
+                c.next = a
+                return
+            c = c.next
+         print("not found")
